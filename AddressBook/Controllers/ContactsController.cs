09613d2c5f5eaa40f8683e04e33062d6a68582e4
Robert Cvjetković <robertcvjetkovic15@gmail.hr>
2017-11-08 19:37:12 +0100

    Add html helper extensiion that highlights link on navbar on current controller

---
+++ AddressBook/Controllers/ContactsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AddressBook.DataAccessLayer;
+using AddressBook.Models;
+
+namespace AddressBook.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private AddressBookDbContext db = new AddressBookDbContext();
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            var contacts = db.Contacts.Include(c => c.ApplicationUser);
+            return View(contacts.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,ProfilePicPath,Gender,Birthday,Note,Title,Organization,ApplicationUserID,CreatedAt,ModifiedAt")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "FirstName", contact.ApplicationUserID);
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "FirstName", contact.ApplicationUserID);
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,ProfilePicPath,Gender,Birthday,Note,Title,Organization,ApplicationUserID,CreatedAt,ModifiedAt")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicationUserID = new SelectList(db.Users, "Id", "FirstName", contact.ApplicationUserID);
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AddressBook/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AddressBook.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Html helper extension method that compares requested route with the given <param name="value" /> value,
+        /// if a match is found then <param name="attribute" /> attreibute value is returned else empty string is returned.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="value">Controller or action that will be compared with route.</param>
+        /// <param name="attribute">The attribute value that will be returned if match is found.</param>
+        /// <returns>A HtmlString containig the given attribute value if match is found else empty string is returned.</returns>
+        public static IHtmlString RouteIf(this HtmlHelper helper, string value, string attribute)
+        {
+            string currentController =
+                (helper.ViewContext.RequestContext.RouteData.Values["controller"] ?? String.Empty).ToString();
+
+            string currentAction =
+                (helper.ViewContext.RequestContext.RouteData.Values["action"] ?? String.Empty).ToString();
+
+            bool hasController = value.Equals(currentController, StringComparison.InvariantCultureIgnoreCase);
+            bool hasAction = value.Equals(currentAction, StringComparison.InvariantCultureIgnoreCase);
+
+            return hasAction || hasController ? new HtmlString(attribute) : new HtmlString(String.Empty);
+        }
+    }
+}
